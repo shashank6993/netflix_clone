560466ff4acabbb38eddb0ca477ebd32f2f3d452
Shashank Kumar Chaudhary <110607850+shashank6993@users.noreply.github.com>
2025-04-25 00:04:29 +0530

    update

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,9 @@
 export default function AuthLayoutLogin() {
   return (
-    <div className="mt-24 rounded bg-black/80">
+    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+      <form>
+        <h1>Sign Up</h1>
+      </form>
       <h1>AuthLayoutLogin</h1>
     </div>
   );
