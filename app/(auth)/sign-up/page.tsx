f5a02ab9eb83ffb9e87a376a938263e609048603
Shashank Kumar Chaudhary <110607850+shashank6993@users.noreply.github.com>
2025-04-28 01:15:53 +0530

    backend

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
+import GoogleIcon from "../../../public/google.svg";
+import Image from "next/image";
+
 export default function AuthLayoutLogin() {
   return (
     <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
       <form>
-        <h1>Sign Up</h1>
+        <h1 className="text-3xl font-semibold text-white">Sign Up</h1>
+        <div className="space-y-4 mt-5"></div>
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full inline-block"
+        />
+        <Button
+          type="submit"
+          variant="destructive"
+          className="w-full bg-[#e50914]"
+        >
+          Sign Up
+        </Button>
       </form>
-      <h1>AuthLayoutLogin</h1>
+      <div className="text-gray-500 text-sm mt-2">
+        Ohh i think i know you Try
+        <Link className="text-white hover:underline" href="/login">
+          Log in now
+        </Link>
+      </div>
+      <div className="flex w-full justify-center items-center gap-x-3 mt-6">
+        <Button variant="outline" size="icon">
+          <GithubIcon className="w-4 h-4" />
+        </Button>
+        <Button variant="outline" size="icon">
+          <Image src={GoogleIcon} alt="Google icon" className="w-6 h-6" />
+        </Button>
+      </div>
     </div>
   );
 }
